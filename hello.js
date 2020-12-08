1bf6fb12c26f57c484a2f0369a2216b0752d65a3
cjmugs <cjmugs@gmail.com>
2020-12-08 13:22:40 -0600

    new commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#form1').onsubmit = function() {
+        const name = document.querySelector('#name').value;
+        alert(`Thank You for all of your personal information, ${name}!`);
+    };
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+   document.querySelectorAll('button').forEach(function(button) {
+      button.onclick = function (){
+          document.querySelector('#color').style.color = button.dataset.color;
+      }  
+   });
+});
